bad657e50ac3df96d6fccda5f456a4b1a1829039
Alfonso Suárez <alfonsosuarezv@gmail.com>
2021-06-12 11:24:43 +0200

    Subido hello

--- src/com/hello/main/Main.java
+++ src/com/hello/main/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("Adiós mundo!!!");
+
+        System.out.println(hello("Alfonso"));
+    }
+
+    public static String hello(String name){
+
+        return "Hello " + name;
+
     }
 }
